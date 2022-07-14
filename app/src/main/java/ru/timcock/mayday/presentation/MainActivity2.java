99c834cdcb41170ae528ffae383aa6f6917ac0d8
KovalevEgor14 <olegnorilsk77@gmail.com>
2022-07-14 17:01:45 +0500

    prereg

--- app/src/main/java/ru/timcock/mayday/presentation/MainActivity2.java
+++ app/src/main/java/ru/timcock/mayday/presentation/MainActivity2.java
@@ -1,40 +1,31 @@
 package ru.timcock.mayday.presentation;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import ru.timcock.mayday.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
     Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button=findViewById(R.id.buttonContinue1);
+        setContentView(R.layout.activity_main2);
+        button=findViewById(R.id.buttonContinue2);
         button.setOnClickListener(this);
-
     }
-
 
     @Override
     public void onClick(View view) {
         switch(view.getId()){
-            case R.id.buttonContinue1:
-                Intent intent = new Intent(this, MainActivity2.class);
+            case R.id.buttonContinue2:
+                Intent intent = new Intent(this, MainActivity3.class);
                 startActivity(intent);
                 break;
         }
-
     }
 }
