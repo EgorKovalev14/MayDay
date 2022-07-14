42e2029883c629e7333e8907b2352699729fb73a
KovalevEgor14 <olegnorilsk77@gmail.com>
2022-07-15 01:15:58 +0500

    fhh

---
+++ app/src/main/java/ru/timcock/mayday/presentation/CalendarFragment.java
@@ -0,0 +1,37 @@
+package ru.timcock.mayday.presentation;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import ru.timcock.mayday.R;
+
+public class CalendarFragment extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public CalendarFragment() {
+    }
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        TextView q = view.findViewById(R.id.buttonFragment);
+        return view;
+    }
+}
--- app/src/main/java/ru/timcock/mayday/presentation/TaskActivity.java
+++ app/src/main/java/ru/timcock/mayday/presentation/TaskActivity.java
@@ -2,6 +2,8 @@ package ru.timcock.mayday.presentation;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
@@ -20,6 +23,8 @@ public class TaskActivity extends AppCompatActivity implements View.OnClickListe
     BottomNavigationView bottomNavigationView;
     BottomNavigationItemView item1,item2,item3,item4, item5;
     ArrayList<BottomNavigationItemView> array = new ArrayList<>();
+
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +46,32 @@ public class TaskActivity extends AppCompatActivity implements View.OnClickListe
         item3.setChecked(true);
         bottomNavigationView=findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        tabLayout=findViewById(R.id.TL);
+        FragmentContainerView fragment = findViewById(R.id.MainFragment);
+        getSupportFragmentManager().beginTransaction().replace(R.id.MainFragment,new CalendarFragment()).commit();
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch(tab.getPosition()){
+                    case 0:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.MainFragment,new CalendarFragment()).commit();
+                    break;
+                    case 1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.MainFragment,new CalendarFragment()).commit();
+                    break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
     @Override
