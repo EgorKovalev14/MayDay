a26a860070939d248a7d1eab044181e6784ff518
Keworker <79995640807la@gmail.com>
2022-07-15 08:27:24 +0500

    Add search by tags et

--- app/src/main/java/ru/timcock/mayday/presentation/CalendarFragment.java
+++ app/src/main/java/ru/timcock/mayday/presentation/CalendarFragment.java
@@ -12,26 +12,11 @@ import android.widget.TextView;
 import ru.timcock.mayday.R;
 
 public class CalendarFragment extends Fragment {
-
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public CalendarFragment() {
-    }
-
-
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
-        TextView q = view.findViewById(R.id.buttonFragment);
+
         return view;
     }
 }
