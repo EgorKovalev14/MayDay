fd376361a117006ae2a15372ddc51bacf773bc62
KovalevEgor14 <olegnorilsk77@gmail.com>
2022-07-15 13:11:44 +0500

    efjbf

--- app/src/main/java/ru/timcock/mayday/presentation/TelegramActivity.java
+++ app/src/main/java/ru/timcock/mayday/presentation/TelegramActivity.java
@@ -3,6 +3,7 @@ package ru.timcock.mayday.presentation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +14,7 @@ import ru.timcock.mayday.R;
 
 public class TelegramActivity extends AppCompatActivity implements View.OnClickListener {
     Button finishButton;
-    TextView loginTelegram;
+    TextView loginTelegram, skip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +24,24 @@ public class TelegramActivity extends AppCompatActivity implements View.OnClickL
         loginTelegram=findViewById(R.id.textViewLoginTelegram);
         finishButton.setOnClickListener(this);
         loginTelegram.setOnClickListener(this);
+        skip=findViewById(R.id.textViewSkip5);
+        skip.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch(view.getId()){
             case R.id.buttonContinueTelegram:
-                Intent intent = new Intent(this, GoalsActivity.class);
-                startActivity(intent);
+                Intent telegram = new Intent(Intent.ACTION_VIEW , Uri.parse("http://t.me/ddmastermind_bot"));
+                startActivity(telegram);
                 break;
             case R.id.textViewLoginTelegram:
                 Intent intent1=new Intent(this,LoginActivity.class);
                 startActivity(intent1);
+                break;
+            case R.id.textViewSkip5:
+                Intent intent = new Intent(this,GoalsActivity.class);
+                startActivity(intent);
                 break;
         }
     }
